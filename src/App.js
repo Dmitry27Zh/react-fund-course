0cfac6a725cfdd033a80347356bb7b99e5c67efa
Dmitry <dmitry27zh@gmail.com>
2024-02-27 16:38:01 +0500

    Create PostItem with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import Counter from './component/Counter'
 import ClassCounter from './component/ClassCounter'
+import './styles/App.css'
+import PostItem from './component/PostItem'
 
 function App() {
   const [value, setValue] = useState('Text')
 
   return (
     <div className="App">
+      <PostItem post={{ id: 1, title: 'JS', body: 'description' }} />
+      <PostItem post={{ id: 2, title: 'JS', body: 'description' }} />
+      <PostItem post={{ id: 3, title: 'JS', body: 'description' }} />
       <h2>{value}</h2>
       <input value={value} onChange={(event) => setValue(event.target.value)} />
       <Counter />
