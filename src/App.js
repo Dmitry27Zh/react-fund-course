15bded9092201393997dd5f36eb5b71f1f54a41b
Dmitry <dmitry27zh@gmail.com>
2024-02-27 15:56:21 +0500

    Use state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useState } from 'react'
+
 function App() {
-  return <div className="App">Hi!</div>
+  const [likes, setLikes] = useState(5)
+  function increment() {
+    setLikes(likes + 1)
+  }
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  )
 }
 
 export default App
