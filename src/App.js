06793281b8735e4be24aac4ac1b650803197d483
Dmitry <dmitry27zh@gmail.com>
2024-02-29 12:46:46 +0500

    Create custom select component, sort posts

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import { useState } from 'react'
-import Counter from './component/Counter'
-import ClassCounter from './component/ClassCounter'
 import './styles/App.css'
 import PostsList from './component/PostsList'
 import PostForm from './component/PostForm'
+import MySelect from './component/UI/select/MySelect'
 
 function App() {
-  const [value, setValue] = useState('Text')
   const [posts, setPosts] = useState([
-    { id: 1, title: 'JS', body: 'description' },
-    { id: 2, title: 'JS 2', body: 'description' },
-    { id: 3, title: 'JS 3', body: 'description' },
+    { id: 1, title: 'ghl', body: '3description' },
+    { id: 2, title: 'def', body: '1description' },
+    { id: 3, title: 'abc', body: '2description' },
   ])
+  const [selectedSort, setSelectedSort] = useState('')
   const addNewPost = (newPost) => {
     setPosts([...posts, newPost])
   }
   const removePost = (post) => {
     setPosts(posts.filter((existingPost) => existingPost.id !== post.id))
   }
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className="App">
       <PostForm addNewPost={addNewPost} />
-      <PostsList posts={posts} title="List 1" removePost={removePost} />
-      <PostsList posts={posts} title="List 2" removePost={removePost} />
-      <h2>{value}</h2>
-      <input value={value} onChange={(event) => setValue(event.target.value)} />
-      <Counter />
-      <Counter />
-      <ClassCounter />
+      {posts.length === 0 ? 'No posts...' : <PostsList posts={posts} title="List 1" removePost={removePost} />}
+      <hr style={{ margin: '15px 0' }} />
+      <div>
+        <MySelect
+          defaultValue="Sort by"
+          options={[
+            { value: 'title', name: 'Name' },
+            { value: 'body', name: 'Description' },
+          ]}
+          value={selectedSort}
+          onChange={sortPosts}
+        />
+      </div>
     </div>
   )
 }
