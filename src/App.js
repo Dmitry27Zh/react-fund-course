e20ee4199934e5c34e6530d4e8129213e1f6a6a2
Dmitry <dmitry27zh@gmail.com>
2024-03-03 11:56:12 +0500

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Posts from './pages/Posts'
+import { Route, Routes } from 'react-router-dom'
 import './styles/App.css'
+import About from './pages/About'
+import Posts from './pages/Posts'
 
 function App() {
   return (
     <div className="App">
-      <Posts />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
     </div>
   )
 }
