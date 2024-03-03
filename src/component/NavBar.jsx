9e21bf6eb2ca677f8cedfe0a92b62e6facf80ec1
Dmitry <dmitry27zh@gmail.com>
2024-03-03 16:03:37 +0500

    Add logout

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom'
+import MyButton from './UI/button/MyButton'
+import { useContext } from 'react'
+import { AuthContext } from '../context'
 
 const NavBar = () => {
+  const { setIsAuth } = useContext(AuthContext)
+
   return (
     <div className="navbar">
+      <MyButton onClick={() => setIsAuth(false)}>Logout</MyButton>
       <div className="navbar__links">
         <Link className="navbar__link" to="/about">
           About
