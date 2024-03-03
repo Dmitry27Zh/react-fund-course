efc126b9e97b1ec106d3b7442514d20aab8631c1
Dmitry <dmitry27zh@gmail.com>
2024-03-03 15:27:58 +0500

    Redirect to Posts

--- src/component/AppRouter.jsx
+++ src/component/AppRouter.jsx
@@ -11,6 +11,7 @@ const AppRouter = () => {
       <Route path="/posts" element={<Posts />} />
       <Route path="/posts/:id" element={<Post />} />
       <Route path="/error" element={<Error />} />
+      <Route path="/" element={<Navigate to="/posts" />} />
       <Route path="*" element={<Navigate to="/error" />} />
     </Routes>
   )
