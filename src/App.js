b6766b8b368c6c0a524431f5dae7e3565bc545c4
Dmitry <dmitry27zh@gmail.com>
2024-03-03 14:55:19 +0500

    Extract AppRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
 import './styles/App.css'
-import About from './pages/About'
-import Posts from './pages/Posts'
-import Error from './pages/Error'
 import NavBar from './component/NavBar'
+import AppRouter from './component/AppRouter'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/error" element={<Error />} />
-        <Route path="*" element={<Navigate to="/error" />} />
-      </Routes>
+      <AppRouter />
     </div>
   )
 }
