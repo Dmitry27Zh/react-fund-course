c53f93864a579ea62d8ac5635f76da3ffa79ac61
Dmitry <dmitry27zh@gmail.com>
2024-03-03 12:07:21 +0500

    Create Error page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './styles/App.css'
 import About from './pages/About'
 import Posts from './pages/Posts'
+import Error from './pages/Error'
 import NavBar from './component/NavBar'
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/posts" element={<Posts />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Navigate to="/error" />} />
       </Routes>
     </div>
   )
---
+++ src/pages/Error.jsx
@@ -0,0 +1,5 @@
+const Error = () => {
+  return <h1 style={{ color: 'red' }}>Error</h1>
+}
+
+export default Error
